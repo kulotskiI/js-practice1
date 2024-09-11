2abda7a04ba7a8f777775878f10c953f69337a21
kulotskiI <ishielmishma24@gmail.com>
2024-09-11 15:22:39 +0800

    initial commit

---
+++ app.js
@@ -0,0 +1,46 @@
+console.log('Hello, World!');
+let name = ' Ishiel Mishma Ganaban';
+console.log('My name is ' + name);
+
+console.log('USING VARIABLES');
+let age = "19";
+console.log("Hello! I am" + name +"," + age + "years old");
+
+console.log("USING UNMBERS");
+
+const randomNumber = 24;
+const randomNumber2 = 10;
+const sum = randomNumber / randomNumber2;
+console.log(sum);
+
+console.log("BOOLEANS");
+
+let a = 24;
+let b = 50;
+let isLessthan = a > b;
+let isGreaterthan = a < b;
+console.log(isLessthan);
+console.log(isGreaterthan);
+
+console.log("COMPARISON");
+
+let x = 60;
+let y = 70;
+
+//Use comparison operators and store the reslt in variables
+let isXGreaterThanY = x > y;
+let isXLessthanY = x < y;
+let isXEqalToY = x === y;
+let isXNotEqualToY = x !== y;
+
+console.log('Is X greater than Y?', isXGreaterThanY);
+console.log('Is X less than Y?', isXLessthanY);
+console.log('Is X eqal to Y?', isXEqalToY);
+console.log('Is X not greater than Y?', isXNotEqualToY);
+
+console.log("STRING");
+
+let firstName = "Ishiel";
+let lastName = "Ganaban";
+let fullName = firstName + "" + lastName;
+console.log(fullName);
